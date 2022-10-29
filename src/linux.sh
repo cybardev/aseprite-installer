500d8964cf5b3c4bf32c868c671b67be62100658
Sheikh <sheikh@cybar.dev>
2022-10-28 22:35:26 -0300

    fix curl and unzip command syntax

--- src/linux.sh
+++ src/linux.sh
@@ -7,21 +7,21 @@ exit 0
 # --- script code --- #
 
 install_deps() {
-echo "Installing dependencies for compilation..."
-echo "Which package manager do you have?"
-echo
-echo "0) skip (dependencies already installed)"
-echo "1) apt"
-echo "2) dnf"
-echo "3) pacman"
-echo "4) none of the above"
-echo
-echo "q) exit script"
-echo
-read -n 1 -p "Enter a number: "
+    echo "Installing dependencies for compilation..."
+    echo "Which package manager do you have?"
+    echo
+    echo "0) skip (dependencies already installed)"
+    echo "1) apt"
+    echo "2) dnf"
+    echo "3) pacman"
+    echo "4) none of the above"
+    echo
+    echo "q) exit script"
+    echo
+    read -n 1 -p "Enter a number: "
 
-case $REPLY in
-    q|Q) # exit the script
+    case $REPLY in
+    q | Q) # exit the script
         exit 0
         ;;
     0) # dependencies installed
@@ -40,34 +40,36 @@ case $REPLY in
         echo "Sorry, we cannot install dependencies using your package manager yet."
         echo "Please install the dependencies and select the appropriate option from the previous menu."
         ;;
-esac
+    esac
 }
 
 install_aseprite() {
-# aseprite installation directory
-ASEPRITE_DIR="$HOME/.local/lib/aseprite"
+    # aseprite installation directory
+    ASEPRITE_DIR="$HOME/.local/lib/aseprite"
 
-# compilation process
-cd "~/.local/lib/"
-git clone --recursive https://github.com/aseprite/aseprite.git
-cd aseprite
-curl "https://github.com/aseprite/skia/releases/download/latest/Skia-Linux-Release-x64-libc++.zip" | unzip -q
-mkdir build
-cd build
-export CC=clang
-export CXX=clang++
-cmake \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
-  -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
-  -DLAF_BACKEND=skia \
-  -DSKIA_DIR=$ASEPRITE_DIR/skia \
-  -DSKIA_LIBRARY_DIR=$ASEPRITE_DIR/skia/out/Release-x64 \
-  -DSKIA_LIBRARY=$ASEPRITE_DIR/skia/out/Release-x64/libskia.a \
-  -G Ninja \
-  ..
-ninja aseprite
+    # compilation process
+    cd "~/.local/lib/"
+    git clone --recursive https://github.com/aseprite/aseprite.git
+    cd aseprite
+    curl -LO $(curl -s https://api.github.com/repos/aseprite/skia/releases/latest | grep "tag_name" | awk '{print "https://github.com/aseprite/skia/releases/download/" substr($2, 2, length($2)-3) "/Skia-Linux-Release-x64-libc++.zip"}')
+    unzip -q "Skia-Linux-Release-x64-libc++.zip" -d "skia"
+    rm "Skia-Linux-Release-x64-libc++.zip"
+    mkdir build
+    cd build
+    export CC=clang
+    export CXX=clang++
+    cmake \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
+        -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
+        -DLAF_BACKEND=skia \
+        -DSKIA_DIR=$ASEPRITE_DIR/skia \
+        -DSKIA_LIBRARY_DIR=$ASEPRITE_DIR/skia/out/Release-x64 \
+        -DSKIA_LIBRARY=$ASEPRITE_DIR/skia/out/Release-x64/libskia.a \
+        -G Ninja \
+        ..
+    ninja aseprite
 
-echo "Compilation Done."
-echo "Please add '$ASEPRITE_DIR/build/bin/' to PATH"
+    echo "Compilation Done."
+    echo "Please add '$ASEPRITE_DIR/build/bin/' to PATH"
 }
--- src/macos.sh
+++ src/macos.sh
@@ -7,79 +7,75 @@ exit 0
 # --- script code --- #
 
 install_deps() {
-# install dependencies for macos
-xcode-select --install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install clang cmake ninja unzip
+    # install dependencies for macos
+    xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install clang cmake ninja unzip
 }
 
 install_aseprite() {
-# aseprite installation directory
-ASEPRITE_DIR="$HOME/.local/lib/aseprite"
+    # aseprite installation directory
+    ASEPRITE_DIR="$HOME/.local/lib/aseprite"
 
-# compilation process
-PLATFORM=$(get_platform)
-cd "~/.local/lib/"
-git clone --recursive https://github.com/aseprite/aseprite.git
-cd aseprite
-curl "https://github.com/aseprite/skia/releases/download/latest/Skia-macOS-Release-$PLATFORM.zip" | unzip -q
-mkdir build
-cd build
-cmake_platform $PLATFORM
-ninja aseprite
+    # compilation process
+    PLATFORM=$(get_platform)
+    cd "~/.local/lib/"
+    git clone --recursive https://github.com/aseprite/aseprite.git
+    cd aseprite
+    curl -LO $(curl -s https://api.github.com/repos/aseprite/skia/releases/latest | grep "tag_name" | awk '{print "https://github.com/aseprite/skia/releases/download/" substr($2, 2, length($2)-3) "/Skia-macOS-Release-$PLATFORM.zip"}')
+    unzip -q "Skia-macOS-Release-$PLATFORM.zip" -d "skia"
+    rm "Skia-macOS-Release-$PLATFORM.zip"
+    mkdir build
+    cd build
+    cmake_platform $PLATFORM
+    ninja aseprite
 
-echo "Compilation Done."
-echo "Please add '$ASEPRITE_DIR/build/bin/' to PATH"
+    echo "Compilation Done."
+    echo "Please add '$ASEPRITE_DIR/build/bin/' to PATH"
 }
 
 get_platform() {
-echo "Which CPU do you have?"
-echo
-echo "1) Apple Silicon (arm64)"
-echo "2) Intel         (x86_64)"
-echo
-read -n 1 -p "Enter a number: "
-case $REPLY in
-    1) return "arm64"
-    ;;
-    2) return "x64"
-    ;;
+    echo "Which CPU do you have?"
+    echo
+    echo "1) Apple Silicon (arm64)"
+    echo "2) Intel         (x86_64)"
+    echo
+    read -n 1 -p "Enter a number: "
+    case $REPLY in
+    1)
+        return "arm64"
+        ;;
+    2)
+        return "x64"
+        ;;
     *) # invalid option
         echo "Unrecognized option. Please try again."
         get_platform
-    ;;
-esac
+        ;;
+    esac
 }
 
 cmake_platform() {
-case $1 in
-    arm64) # arm
-cmake \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_OSX_ARCHITECTURES=arm64 \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
-  -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
-  -DLAF_BACKEND=skia \
-  -DSKIA_DIR=$HOME/deps/skia \
-  -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-arm64 \
-  -DSKIA_LIBRARY=$HOME/deps/skia/out/Release-arm64/libskia.a \
-  -DPNG_ARM_NEON:STRING=on \
-  -G Ninja \
-  ..
-    ;;
-    x64) # intel
-cmake \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_OSX_ARCHITECTURES=arm64 \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
-  -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
-  -DLAF_BACKEND=skia \
-  -DSKIA_DIR=$HOME/deps/skia \
-  -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-arm64 \
-  -DSKIA_LIBRARY=$HOME/deps/skia/out/Release-arm64/libskia.a \
-  -DPNG_ARM_NEON:STRING=on \
-  -G Ninja \
-  ..
-    ;;
-esac
+    case $1 in
+    arm64)
+        P="arm64"
+        EXTRA="-DPNG_ARM_NEON:STRING=on"
+        ;;
+    x64)
+        P="x86_64"
+        EXTRA=""
+        ;;
+    esac
+    cmake \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_OSX_ARCHITECTURES=$P \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+        -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
+        -DLAF_BACKEND=skia \
+        -DSKIA_DIR=$HOME/deps/skia \
+        -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-$1 \
+        -DSKIA_LIBRARY=$HOME/deps/skia/out/Release-arm64/libskia.a \
+        $EXTRA \
+        -G Ninja \
+        ..
 }
