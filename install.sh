3e76e9b51266c37f5b33fe587148ad4f198e2f26
cybardev <sheikh@cybar.dev>
2024-01-02 11:04:48 -0400

    fix: use clang and libc metapackage instead of "-10" packages not available in repo

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ if [ -d "$ASEPRITE_DIR" ]; then
 fi
 
 # install dependencies
-sudo apt-get install -y g++ clang-10 libc++-10-dev libc++abi-10-dev cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev unzip
+sudo apt-get install -y g++ clang libc++-dev libc++abi-dev cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev unzip
 
 # save current working directory
 CURRENT_DIR="$(pwd)"
