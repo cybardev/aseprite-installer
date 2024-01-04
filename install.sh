8fee5ee326979abdfc2403d82622133bae69964a
Sheikh <sheikh@cybar.dev>
2024-01-03 22:36:39 -0400

    feat: add progress logs at significant points

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ if [ -d "$ASEPRITE_DIR" ]; then
 fi
 
 # install dependencies
+echo "\e[1;36m[INFO]\e[0m Installing dependencies..."
 sudo apt-get install -y g++ clang libc++-dev libc++abi-dev cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev unzip
 
 # save current working directory
@@ -28,15 +29,18 @@ CURRENT_DIR="$(pwd)"
 cd "$LIB"
 
 # clone aseprite recursively (include submodules)
+echo "\e[1;36m[INFO]\e[0m Downloading Aseprite source files..."
 git clone --recursive https://github.com/aseprite/aseprite.git
 cd aseprite
 
 # download and unzip skia prebuilt for aseprite
+echo "\e[1;36m[INFO]\e[0m Downloading Skia for Aseprite..."
 curl -LO $(curl -s https://api.github.com/repos/aseprite/skia/releases/latest | grep "tag_name" | awk '{print "https://github.com/aseprite/skia/releases/download/" substr($2, 2, length($2)-3) "/Skia-Linux-Release-x64-libc++.zip"}')
 unzip -q "Skia-Linux-Release-x64-libc++.zip" -d "skia"
 rm "Skia-Linux-Release-x64-libc++.zip"
 
-# compilation process
+# main build process
+echo "\e[1;36m[INFO]\e[0m Starting build process..."
 mkdir build
 cd build
 export CC=clang
@@ -52,6 +56,8 @@ cmake \
     -G Ninja \
     ..
 ninja aseprite
+
+echo "\e[1;36m[INFO]\e[0m Integrating Aseprite with the system..."
 
 # symlink the binary to a location on PATH for CLI access
 [ ! -d "$BIN" ] && mkdir -p "$BIN"
@@ -75,8 +81,7 @@ printf "%s\n" > "$APP/aseprite.desktop" \
     "MimeType=image/bmp;image/gif;image/jpeg;image/png;image/x-pcx;image/x-tga;image/vnd.microsoft.icon;video/x-flic;image/webp;image/x-aseprite;"
 
 # installation complete message
-echo
-echo -e "\e[1;32m[DONE]\e[1;33m Aseprite is now installed. Enjoy~ :3\e[0m"
+echo -e "\n\e[1;32m[DONE]\e[1;33m Aseprite is now installed. Enjoy~ :3\e[0m"
 
 # go back to saved working directory
 cd "$CURRENT_DIR"
